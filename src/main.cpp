ce1232855a2d1d4323ca8ad679401fc92d0ca703
ChrisMoutsos <ckm13d@my.fsu.edu>
2015-01-29 16:00:51 -0500

    ...

--- src/main.cpp
+++ src/main.cpp
@@ -12,6 +12,10 @@
  *	Encode moveFrom when I copy pieceMoveLists to global moveLists
  *
  *	Stalemates (check if implemented correctly), draw by material
+
+
+
+
  *	prevOnMoveTo needs to be a vector
  *	
  * 	Castling, en passants, promotions
