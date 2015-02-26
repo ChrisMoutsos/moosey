3b1973eda364a324d3cf62b9849bf18fabcbdb38
ChrisMoutsos <ckm13d@my.fsu.edu>
2015-02-26 18:44:17 -0500

    Removed unnecessary externs and forward declarations

--- include/sdl.h
+++ include/sdl.h
@@ -8,8 +8,6 @@
 #ifndef SDL_H
 #define SDL_H
 
-class LTexture;
-
 const int SCREEN_W = 1250;
 const int SCREEN_H = 700;
 
@@ -19,7 +17,6 @@ void close_SDL();
 
 extern SDL_Window* window;
 extern SDL_Renderer* renderer;
-extern LTexture spriteSheetTexture, undoTexture, restartTexture;
 extern SDL_Color textColor;
 
 #endif
