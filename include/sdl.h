27a8d88636c83e39095cf5c59d2b4ea9cb1cf06c
ChrisMoutsos <ckm13d@my.fsu.edu>
2015-02-26 13:58:00 -0500

    Changed window width

--- include/sdl.h
+++ include/sdl.h
@@ -10,7 +10,7 @@
 
 class LTexture;
 
-const int SCREEN_W = 1200;
+const int SCREEN_W = 1250;
 const int SCREEN_H = 700;
 
 bool init_SDL();
@@ -19,7 +19,7 @@ void close_SDL();
 
 extern SDL_Window* window;
 extern SDL_Renderer* renderer;
-extern LTexture spriteSheetTexture;
+extern LTexture spriteSheetTexture, undoTexture, restartTexture;
 extern SDL_Color textColor;
 
 #endif
