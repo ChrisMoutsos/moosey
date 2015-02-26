f9eed7d2c58403bd557183a52e818496a9aa8067
ChrisMoutsos <ckm13d@my.fsu.edu>
2015-02-26 18:43:36 -0500

    Added undo/restart buttons

--- include/display.h
+++ include/display.h
@@ -10,21 +10,24 @@
 
 class Board;
 class Square;
+class Button;
+
+extern Button buttons[2];
 
 const int B_SIZE = 600;
 const int SQ_SIZE = 75;
+const int BUT_SIZEX = 51;
+const int BUT_SIZEY = 31;
 const int BXSTART = 50;
 const int BYSTART = 25;
 
 void displayBoard(Board& b, const int& mF = -1, const int& mT = -1);
-void setPiecesOnSquares(const Board& b);
-void setSquarePositions();
+void setButtonPositions();
 void setSpriteClips();
+void drawButtons(const Board& b);
 void drawSquares(const Board& b, const int& mF = -1, const int& mT = -1);
 void drawPieces(const Board& b, const int& mF = -1, const int& mT = -1);
 void drawBorder();
 void drawMoveTable(const Board&);
-
-extern Square squares[64];
 
 #endif
