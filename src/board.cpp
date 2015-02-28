930cbd299f32602e3b9a872c88a79a3cc2a48eaf
Chris Moutsos <ckm13d@my.fsu.edu>
2015-02-28 18:42:22 -0500

    Updated board.cpp to comply with Piece class

--- src/board.cpp
+++ src/board.cpp
@@ -33,11 +33,6 @@ Board::Board(std::string FEN) {
 	//FEN stuff
 }
 
-Board::~Board() {
-	for (int i = wqR; i <= bPh; i++) 
-		delete [] piece[i].moveList;
-}
-
 void Board::setSquarePositions() {
 	for (int i = 0; i < 64; i++) {
 		squares[i].setPos(BXSTART+(SQ_SIZE*(i%8)),
@@ -71,7 +66,7 @@ void Board::emptyBoard() {
 }
 
 void Board::placePiece(int p, int sq) {
-	piece[p].pos = sq;
+	piece[p].setPos(sq);
 	board120[sq] = p;
 }
 
@@ -87,51 +82,70 @@ void Board::placePiecesDefault() {
 }
 
 void Board::initializePieces() {
-        int v;
-	pieceEntry pieceTemp[32] = {
-		{"Rook", 'R', R_VAL, 0, 0, 1, 0, 1},{"Knight", 'N', N_VAL, 0, 0, 1, 0, 1}, 
-		{"Bishop", 'B', B_VAL, 0, 0, 1, 0, 1}, {"Queen", 'Q', Q_VAL, 0, 0, 1, 0, 1}, 
-		{"King", 'K', K_VAL, 0, 0, 1, 0, 1}, {"Bishop", 'B', B_VAL, 0, 0, 1, 0, 1},
-		{"Knight", 'N', N_VAL, 0, 0, 1, 0, 1}, {"Rook", 'R', R_VAL, 0, 0, 1, 0, 1}, 
-		{"Pawn", 'P', P_VAL, 0, 0, 1, 0, 1}, {"Pawn", 'P', P_VAL, 0, 0, 1, 0, 1}, 
-		{"Pawn", 'P', P_VAL, 0, 0, 1, 0,1 }, {"Pawn", 'P', P_VAL, 0, 0, 1, 0, 1}, 
-		{"Pawn", 'P', P_VAL, 0, 0, 1, 0, 1}, {"Pawn", 'P', P_VAL, 0, 0, 1, 0, 1}, 
-		{"Pawn", 'P', P_VAL, 0, 0, 1, 0, 1}, {"Pawn", 'P', P_VAL, 0, 0, 1, 0, 1}, 
-		{"Rook", 'r', R_VAL, 0, 0, 1, 0, 0}, {"Knight", 'n', N_VAL, 0, 0, 1, 0, 0}, 
-		{"Bishop", 'b', B_VAL, 0, 0, 1, 0, 0}, {"Queen", 'q', Q_VAL, 0, 0, 1, 0, 0}, 
-		{"King", 'k', K_VAL, 0, 0, 1, 0, 0}, {"Bishop", 'b', B_VAL, 0, 0, 1, 0, 0}, 
-		{"Knight", 'n', N_VAL, 0, 0, 1, 0, 0}, {"Rook", 'r', R_VAL, 0, 0, 1, 0, 0}, 
-		{"Pawn", 'p', P_VAL, 0, 0, 1, 0, 0}, {"Pawn", 'p', P_VAL, 0, 0, 1, 0, 0}, 
-		{"Pawn", 'p', P_VAL, 0, 0, 1, 0, 0}, {"Pawn", 'p', P_VAL, 0, 0, 1, 0, 0},
-		{"Pawn", 'p', P_VAL, 0, 0, 1, 0, 0}, {"Pawn", 'p', P_VAL, 0, 0, 1, 0, 0}, 
-		{"Pawn", 'p', P_VAL, 0, 0, 1, 0, 0}, {"Pawn", 'p', P_VAL, 0, 0, 1, 0, 0}
-        };
-	for (int i = 0; i < 32; i++)
-		piece[i] = pieceTemp[i];
+	int v;
 
-	for (int i = wqR; i <= bPh; i++) {
-		v = piece[i].value;
-		switch (v) {
-		case K_VAL:
-			piece[i].moveListSize = 8;
-			break;
-		case Q_VAL:
-			piece[i].moveListSize = 27;
-			break;
-		case R_VAL:
-			piece[i].moveListSize = 14;
-			break;
-		case B_VAL:
-			piece[i].moveListSize = 13;
-			break;
-		case N_VAL:
-			piece[i].moveListSize =8;
-			break;
-		case P_VAL:
-			piece[i].moveListSize = 4;
-			break;
+	for (int i = 0; i <= 16; i += 16) {
+		piece[wqR+i].setName("Rook");
+		piece[wqR+i].setAbbr(char(int('R') + 2*i));
+		piece[wqR+i].setValue(R_VAL);
+
+		piece[wqN+i].setName("Knight");
+		piece[wqN+i].setAbbr(char(int('N') + 2*i));
+		piece[wqN+i].setValue(N_VAL);
+
+		piece[wqB+i].setName("Bishop");
+		piece[wqB+i].setAbbr(char(int('B') + 2*i));
+		piece[wqB+i].setValue(B_VAL);
+
+		piece[wQ+i].setName("Queen");
+		piece[wQ+i].setAbbr(char(int('Q') + 2*i));
+		piece[wQ+i].setValue(Q_VAL);
+
+		piece[wK+i].setName("King");
+		piece[wK+i].setAbbr(char(int('K') + 2*i));
+		piece[wK+i].setValue(K_VAL);
+
+		piece[wkB+i].setName("Bishop");
+		piece[wkB+i].setAbbr(char(int('B') + 2*i));
+		piece[wkB+i].setValue(B_VAL);
+
+		piece[wkN+i].setName("Knight");
+		piece[wkN+i].setAbbr(char(int('N') + 2*i));
+		piece[wkN+i].setValue(N_VAL);
+
+		piece[wkR+i].setName("Rook");
+		piece[wkR+i].setAbbr(char(int('R') + 2*i));
+		piece[wkR+i].setValue(R_VAL);
+	}
+	for (int s = 0; s <= 16; s += 16) 
+		for (int i = 0; i < 8; i++) { 
+			piece[wPa+i+s].setName("Pawn");
+			piece[wPa+i+s].setAbbr(char(int('P') + 2*s));
+			piece[wPa+i+s].setValue(P_VAL);
 		}
-		piece[i].moveList = new int[piece[i].moveListSize];
+
+	for (int i = wqR; i <= wPh; i++)
+		piece[i].setColor(WHITE);
+	for (int i = bqR; i <= bPh; i++)
+		piece[i].setColor(BLACK);
+	
+
+	//Create moveLists
+	for (int i = wqR; i <= bPh; i++) {
+		v = piece[i].getValue();
+		if (v == K_VAL)
+			piece[i].setMoveListSize(8);
+		else if (v == Q_VAL)
+			piece[i].setMoveListSize(27);
+		else if (v == R_VAL)
+			piece[i].setMoveListSize(14);
+		else if (v == B_VAL)
+			piece[i].setMoveListSize(13);
+		else if (v == N_VAL)
+			piece[i].setMoveListSize(8);
+		else if (v == P_VAL)
+			piece[i].setMoveListSize(4);
+		piece[i].setMoveList(new int[piece[i].getMoveListSize()]);
     }
 }
 
@@ -163,7 +177,6 @@ void Board::handleInput(int& mF, int& mT, SDL_Event* e) {
 }
 
 //ACCESSORS
-//---Board
 int Board::getBoard120(int sq) const {
 	return (sq > -1 && sq < 99) ? board120[sq] : invalid;
 }
@@ -200,43 +213,8 @@ int Board::getPrevOnMoveTo(int i) const {
 int Board::getPieceMoved(int i) const {
 	return (i > -1 && i < (int)pieceMoved.size()) ? pieceMoved[i] : -1;
 }
-//---Pieces
-char Board::getPieceAbbr(int p) const {
-	return (p >= wqR && p <= bPh) ? piece[p].abbr : '0';
-}
-
-int Board::getTimesMoved(int p) const {
-	return (p >= wqR && p <= bPh) ? piece[p].moved : -1;
-}
-
-std::string Board::getName(int p) const {
-	return (p >= wqR && p <= bPh) ? piece[p].name : "0";
-}
-
-bool Board::getAlive(int p) const {
-	return (p >= wqR && p <= bPh) ? piece[p].alive : -1;
-}
-
-int Board::getFromPieceMoveList(int p, int i) const {
-	if (!(p >= wqR && p <= bPh)) return -1;
-	if (!(i > -1 && i < piece[p].moveListSize)) return -1;
-	return piece[p].moveList[i];
-}
-
-int Board::getPieceMoveListSize(int p) const {
-	return (p >= wqR && p <= bPh) ? piece[p].moveListSize : -1;
-}
-
-int Board::getValue(int p) const {
-	return (p >= wqR && p <= bPh) ? piece[p].value : -1;
-}
-
-int Board::getPos(int p) const {
-	return (p >= wqR && p <= bPh) ? piece[p].pos : -1;
-}
 
 //MUTATORS
-//---Board
 void Board::setBoard120(int i, int v) {
 	if (i > -1 && i < 120) board120[i] = v;
 }
@@ -249,24 +227,4 @@ void Board::addToMovelist(bool s, int v) {
 void Board::clearMoveList(bool s) {
 	if (s) whiteMoveList.clear();
 	else blackMoveList.clear();
-}
-//---Pieces
-void Board::killPiece(int p) {
-	if (p >= wqR && p <= bPh) piece[p].alive = false;
-}
-
-void Board::unkillPiece(int p) {
-	if (p >= wqR && p <= bPh) piece[p].alive = true;
-}
-
-void Board::setPiecePos(int p, int newPos) {
-	if (p >= wqR && p <= bPh) piece[p].pos = newPos;
-}
-
-void Board::incrMoved(int p) {
-	if (p >= wqR && p <= bPh) piece[p].moved++;
-}
-
-void Board::decrMoved(int p) {
-	if (p >= wqR && p <= bPh) piece[p].moved--;
 }
