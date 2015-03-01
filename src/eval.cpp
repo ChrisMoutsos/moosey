58cc080c455ce1ca0b349922fff4fc4ef94df2f5
Chris Moutsos <ckm13d@my.fsu.edu>
2015-03-01 17:01:47 -0500

    Using piece-square tables in eval.cpp

--- src/eval.cpp
+++ src/eval.cpp
@@ -6,10 +6,133 @@
 */
 
 #include "board.h"
+#include <iostream>
 
 int Board::eval() {
+	static int pawnTable[64] = { 0,  0,  0,  0,  0,  0,  0,  0,
+				    50, 50, 50, 50, 50, 50, 50, 50,
+				    10, 10, 20, 30, 30, 20, 10, 10,
+				     5,  5, 10, 25, 25, 10,  5,  5,
+				     0,  0,  0, 20, 20,  0,  0,  0,
+				     5, -5,-10,  0,  0,-10, -5,  5,
+				     5, 10, 10,-20,-20, 10, 10,  5,
+				     0,  0,  0,  0,  0,  0,  0,  0 };
+	static int knightTable[64] = { -50,-40,-30,-30,-30,-30,-40,-50,
+				       -40,-20,  0,  0,  0,  0,-20,-40,
+				       -30,  0, 10, 15, 15, 10,  0,-30,
+				       -30,  5, 15, 20, 20, 15,  5,-30,
+				       -30,  0, 15, 20, 20, 15,  0,-30,
+				       -30,  5, 10, 15, 15, 15,  5,-30,
+				       -40,-20,  0,  5,  5,  0,-20,-40,
+			               -50,-40,-30,-30,-30,-30,-40,-50 }; 
+	static int bishopTable[64] = { -20,-10,-10,-10,-10,-10,-10,-20,
+				       -10,  0,  0,  0,  0,  0,  0,-10,
+				       -10,  0,  5, 10, 10,  5,  0,-10,
+				       -10,  5,  5, 10, 10,  5,  5,-10,
+	                               -10,  0, 10, 10, 10, 10,  0,-10,
+				       -10, 10, 10, 10, 10, 10, 10,-10,
+				       -10,  5,  0,  0,  0,  0,  5,-10,
+				       -20,-10,-10,-10,-10,-10,-10,-20 };
+	static int rookTable[64] = { 0,  0,  0,  0,  0,  0,  0,  0,
+				     5, 10, 10, 10, 10, 10, 10,  5,
+				    -5,  0,  0,  0,  0,  0,  0, -5,
+				    -5,  0,  0,  0,  0,  0,  0, -5,
+				    -5,  0,  0,  0,  0,  0,  0, -5,
+				    -5,  0,  0,  0,  0,  0,  0, -5,
+				    -5,  0,  0,  0,  0,  0,  0, -5,
+	  			     0,  0,  0,  5,  5,  0,  0,  0 };
+	static int queenTable[64] = { -20,-10,-10, -5, -5,-10,-10,-20,
+				      -10,  0,  0,  0,  0,  0,  0,-10,
+				      -10,  0,  5,  5,  5,  5,  0,-10,
+				       -5,  0,  5,  5,  5,  5,  0, -5,
+				        0,  0,  5,  5,  5,  5,  0, -5,
+				      -10,  5,  5,  5,  5,  5,  0,-10,
+				      -10,  0,  5,  0,  0,  0,  0,-10,
+				      -20,-10,-10, -5, -5,-10,-10,-20 };
+	static int kingTable1[64] = { -30,-40,-40,-50,-50,-40,-40,-30,
+				      -30,-40,-40,-50,-50,-40,-40,-30,
+				      -30,-40,-40,-50,-50,-40,-40,-30,
+				      -30,-40,-40,-50,-50,-40,-40,-30,
+				      -20,-30,-30,-40,-40,-30,-30,-20,
+	 			      -10,-20,-20,-20,-20,-20,-20,-10,
+				       20, 20,  0,  0,  0,  0, 20, 20,
+				       20, 30, 10,  0,  0, 10, 30, 20 };
+	static int kingTable2[64] = { -50,-40,-30,-20,-20,-30,-40,-50,
+				      -30,-20,-10,  0,  0,-10,-20,-30,
+				      -30,-10, 20, 30, 30, 20,-10,-30,
+				      -30,-10, 30, 40, 40, 30,-10,-30,
+				      -30,-10, 30, 40, 40, 30,-10,-30,
+				      -30,-10, 20, 30, 30, 20,-10,-30,
+				      -30,-30,  0,  0,  0,  0,-30,-30,
+				      -50,-30,-30,-30,-30,-30,-30,-50 };
+
 	int score = 0;
-	
+
+	//Piece-square table
+	//---White pieces
+	int index, posi;
+	for (int i = wqR; i <= wkR; i += wkR-wqR)
+		if (piece[i].getAlive()) {
+			posi = to64(piece[i].getPos())-1;
+			index = posi+56 - posi/8*16;
+			score += rookTable[index];
+		}
+	for (int i = wqN; i <= wkN; i += wkN-wqN)
+		if (piece[i].getAlive()) {
+			posi = to64(piece[i].getPos())-1;
+			index = posi+56 - posi/8*16;
+			score += knightTable[index];
+		}
+	for (int i = wqB; i <= wkB; i += wkB-wqB)
+		if (piece[i].getAlive()) {
+			posi = to64(piece[i].getPos())-1;
+			index = posi+56 - posi/8*16;
+			score += bishopTable[index];
+		}
+	if (piece[wQ].getAlive()) {
+		posi = to64(piece[wQ].getPos())-1;
+		index = posi+56 - posi/8*16;
+		score += queenTable[index];
+	}
+	if (piece[wK].getAlive()) {
+		posi = to64(piece[wK].getPos())-1;
+		index = posi+56 - posi/8*16;
+		score += kingTable1[index];
+	}
+	for (int i = wPa; i <= wPh; i++) {
+		if (piece[i].getAlive()) {
+			posi = to64(piece[i].getPos())-1;
+			index = posi+56 - posi/8*16;
+			if (piece[i].getValue() == P_VAL)
+				score += pawnTable[index];
+			else
+				score += queenTable[index];
+		}
+	}
+	//---Black pieces
+	for (int i = bqR; i <= bkR; i += bkR-bqR)
+		if (piece[i].getAlive())
+			score -= rookTable[to64(piece[i].getPos())-1];
+	for (int i = bqN; i <= bkN; i += bkN-bqN)
+		if (piece[i].getAlive())
+			score -= knightTable[to64(piece[i].getPos())-1];
+	for (int i = bqB; i <= bkB; i += bkB-bqB)
+		if (piece[i].getAlive())
+			score -= bishopTable[to64(piece[i].getPos())-1];
+	if (piece[bQ].getAlive())
+		score -= queenTable[to64(piece[bQ].getPos())-1];
+	if (piece[bK].getAlive())
+		score -= kingTable1[to64(piece[bK].getPos())-1];
+	for (int i = bPa; i <= bPh; i++) {
+		if (piece[i].getAlive()) {
+			if (piece[i].getValue() == P_VAL) {
+				score -= pawnTable[to64(piece[i].getPos())-1];
+			}
+			else
+				score -= queenTable[to64(piece[i].getPos())-1];
+		}
+	}
+
 	//Material
 	for (int i = wqR; i <= wPh; i++) {
 		if (piece[i].getAlive())
@@ -20,13 +143,33 @@ int Board::eval() {
 			score -= piece[i].getValue();
 	}
 
-	//Center control
-
 	//Castled
 	if (whiteCastled) score += 300;
 	if (blackCastled) score -= 300;
 
-	//Pawns
+	//Doubled pawns
+	int doubled = 0, pos;
+	for (int i = wPa; i <= wPh; i++) {
+		if (!piece[i].getAlive()) continue;
+		pos = piece[i].getPos();
+		for (int j = wPa; j <= wPh; j++) {
+			if (!piece[j].getAlive()) continue;
+			if (j != i && piece[j].getPos()%10 == pos%10) 
+					doubled++;
+		}
+	}
+	score -= doubled*25;
+	doubled = 0;
+	for (int i = bPa; i <= bPh; i++) {
+		if (!piece[i].getAlive()) continue;
+		pos = piece[i].getPos();
+		for (int j = bPa; j <= bPh; j++) {
+			if (!piece[j].getAlive()) continue;
+			if (j != i && piece[j].getPos()%10 == pos%10) 
+					doubled++;
+		}
+	}
+	score += doubled*25;
 
 	//Open filed rooks
 	for (int j = wqR; j <= bqR; j += (bqR-wqR)) {
@@ -36,13 +179,13 @@ int Board::eval() {
 			int rank = piece[i].getPos()/10;
 			int file = piece[i].getPos()%10;
 			int checkPiece;
-			for (int r = rank; r <= 9; i++) { //Up
+			for (int r = rank; r <= 9; r++) { //Up
 				checkPiece = board120[r*10+file];
 				if (checkPiece != empty)
 					if (piece[checkPiece].getColor() != piece[i].getColor())
 						open = false;
 			}
-			for (int r = rank; r >= 2; i--) { //Down
+			for (int r = rank; r >= 2; r--) { //Down
 				checkPiece = board120[r*10+file];
 				if (checkPiece != empty)
 					if (piece[checkPiece].getColor() != piece[i].getColor())
@@ -54,7 +197,8 @@ int Board::eval() {
 	}
 
 	//Mobility
-	
+	score += (int)whiteMoveList.size();
+	score -= (int)blackMoveList.size();
 
 	return side ? score : -score;
 }
