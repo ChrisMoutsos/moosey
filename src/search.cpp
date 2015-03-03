7d47d62888107abe8716c550c6dace4445ce6dc9
Chris Moutsos <ckm13d@my.fsu.edu>
2015-03-03 18:57:24 -0500

    Move ordering, timer, etc.

--- src/search.cpp
+++ src/search.cpp
@@ -5,35 +5,42 @@
 ----------------------------------
 */
 
+#include <ctime>
 #include "search.h"
 #include "board.h"
 #include <iostream>
+#include "display.h"
+
+int nodes;
 
 int search(Board& b) {
 	using std::vector;
-
+	
+	nodes = 0;
+	const clock_t begin_time = clock();
 	int bestMove = 0, bestScore = -99999, score;
 	int mF, mT;
 	int alpha = -99999, beta = 99999;
 	vector<int> moveList;
 
+	std::cout << "Current eval: " << b.eval() << '\n';
 //	std::cout << "\n\n\n\nNEWSEARCH, BOT SEARCHING FOR: " << b.getSide() << "\n\n";
-	if (b.getSide())
-		moveList = b.whiteMoveList;
-	else
-		moveList = b.blackMoveList;
+	b.generateMoveListFor(b.getSide(), moveList);
+	b.orderMoveList(b.getSide(), moveList);
 
 	for (int i = 0; i < (int)moveList.size(); i++) {
-		alpha = -99999;
-		beta = -alpha;
-
+		//alpha = -99999;
+		//beta = -alpha;
+		//displayBoard(b);
+		//SDL_Delay(100);
+	
 		mF = moveList[i]/100;
 		mT = moveList[i]%100;
 //		std::cout << "Searching ROOT " << i << " mF: " << mF << " mT: " << mT << '\n';
 		b.setMove(mF, mT);
 		b.movePiece();
 
-		score = -alphaBeta(b, alpha, beta, 3);
+		score = -alphaBeta(b, alpha, beta, 5);
 
 		if (score > bestScore) {
 			bestScore = score;
@@ -45,9 +52,13 @@ int search(Board& b) {
 
 		b.unmovePiece();
 		b.changeTurn();
-
+		nodes++;
 	}
+
 	std::cout << "\nBest score : " << bestScore << " Best move : " << bestMove << '\n';
+	std::cout << "Time elapsed: " << float(clock()-begin_time) / CLOCKS_PER_SEC << '\n';
+	std::cout << "Nodes searched: " << nodes << '\n';
+	std::cout << "Nodes/sec: " << nodes / (float(clock()-begin_time) / CLOCKS_PER_SEC) << '\n';
 	
 	if (bestMove == 0) std::cout << "CHECKMATED\n";
 	return bestMove;
@@ -61,24 +72,26 @@ int alphaBeta(Board& b, int alpha, int beta, int depthLeft) {
 	vector<int> moveList;
 
 	b.changeTurn();
-	if (b.getSide()) {
-		b.generateMoveLists(moveList, b.blackMoveList);
-	}
-	else {
-		b.generateMoveLists(b.whiteMoveList, moveList);
-	}
-	b.checkCheck(b.getSide());
+	b.generateMoveListFor(b.getSide(), moveList);
+	b.checkCheckForBot(b.getSide(), moveList);
+	b.orderMoveList(b.getSide(), moveList);
 
 	if (depthLeft == 0) {
 //		std::cout << "Scoring... " << b.eval() << '\n';
 		//b.changeTurn();
+//		displayBoard(b);
 		return b.eval();
 	}
 
 	for (int i = 0; i < (int)moveList.size(); i++) {
+		nodes++;
+
+//		displayBoard(b);
+//		SDL_Delay(100);
+
 		mF = moveList[i]/100;
 		mT = moveList[i]%100;
-		for (int i = 0; i <= depthLeft; i++) std::cout << '\t';
+		//for (int i = 0; i <= depthLeft; i++) std::cout << '\t';
 //		std::cout << "AB depth: " << depthLeft << " mF: " << mF << " mT: " << mT << '\n';
 		b.setMove(mF, mT);
 		b.movePiece();
@@ -86,7 +99,7 @@ int alphaBeta(Board& b, int alpha, int beta, int depthLeft) {
 		score = -alphaBeta(b, -beta, -alpha, depthLeft - 1);
 		b.changeTurn();
 
-		for (int i = 0; i <= depthLeft; i++) std::cout << '\t';
+		//for (int i = 0; i <= depthLeft; i++) std::cout << '\t';
 //		std::cout << "score: " << score << '\n';
 		if (score >= beta) {
 			b.unmovePiece();
