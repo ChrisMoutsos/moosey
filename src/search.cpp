e41f7722e711358c9651ac09e6b2f920e8bd572f
Chris Moutsos <ckm13d@my.fsu.edu>
2015-03-05 00:38:10 -0500

    First version of quies search. Not sure if working

--- src/search.cpp
+++ src/search.cpp
@@ -24,7 +24,7 @@ int think(Board& b, int depth) {
 	LINE prinVarLine;
 
 	b.generateMoveListFor(b.getSide(), moveList);
-	b.checkCheckForBot(b.getSide(), moveList);
+	b.checkCheck(b.getSide(), moveList);
 	b.orderMoveList(b.getSide(), moveList);
 	
 	for (int i = depth; i <= depth; i++) {
@@ -48,28 +48,27 @@ int think(Board& b, int depth) {
 int alphaBeta(Board& b, int alpha, int beta, int depthLeft, LINE* pline) {
 	using std::vector;
 
-	bool foundPV = false;
-	int score, mF, mT;
-	vector<int> moveList;
-
-	LINE line;
-
-	if (depthLeft) {
-		b.generateMoveListFor(b.getSide(), moveList);
-		b.checkCheckForBot(b.getSide(), moveList);
-	//	if (depthLeft > 2)
-	//		b.orderByScore(moveList);
-	//	else
-			if (depthLeft > 0)
-				b.orderMoveList(b.getSide(), moveList);
-	}
-
 	if (depthLeft == 0) {
 		pline->count = 0;
 		terminalNodes++;
 		nodes++;
 		return b.eval();
 	}
+
+	bool foundPV = false;
+	int score, mF, mT;
+	vector<int> moveList;
+
+	LINE line;
+
+	b.generateMoveListFor(b.getSide(), moveList);
+	b.checkCheck(b.getSide(), moveList);
+	//if (depthLeft > 2)
+	//	b.orderByScore(moveList);
+	//else
+	b.orderMoveList(b.getSide(), moveList);
+
+
 
 	for (int i = 0; i < (int)moveList.size(); i++) {
 		vector<int> localPV;
@@ -87,12 +86,11 @@ int alphaBeta(Board& b, int alpha, int beta, int depthLeft, LINE* pline) {
 		}	
 		else
 			score = -alphaBeta(b, -beta, -alpha, depthLeft - 1, &line);
+		b.unmovePiece();
 		b.changeTurn();
 
-		if (score >= beta) {
-			b.unmovePiece();
+		if (score >= beta)
 			return beta;
-		}
 		if (score > alpha) {
 			alpha = score;
 			foundPV = true;
@@ -100,10 +98,48 @@ int alphaBeta(Board& b, int alpha, int beta, int depthLeft, LINE* pline) {
 			memcpy(pline->move + 1, line.move, line.count * sizeof(int));
 			pline->count = line.count + 1;
 		}
-
-		b.unmovePiece();
-
 	}
 	
+	return alpha;
+}
+
+int Quies(Board& b, int alpha, int beta, LINE* pline) {
+	using std::vector;
+
+	LINE line;
+	vector<int> captureList;
+	
+	if (b.checkCheck(b.getSide(), captureList))
+		return alphaBeta(b, alpha, beta, 1, pline); 
+
+	int score = b.eval();
+	int mF, mT;
+	
+	if (score >= beta)
+		return beta;
+	if (score > alpha)
+		alpha = score;
+
+	b.generateGoodCaptures(b.getSide(), captureList); 
+	for (int i = 0; i < (int)captureList.size(); i++) {
+		mF = captureList[i]/100;
+		mT = captureList[i]%100;
+		b.setMove(mF, mT);
+		b.movePiece();
+		b.changeTurn();
+		score = -Quies(b, -beta, -alpha, &line);
+		b.unmovePiece();
+		b.changeTurn();
+		
+		if (score >= beta)
+			return beta;
+		if (score > alpha) {
+			alpha = score;
+			pline->move[0] = mF*100 + mT;
+			memcpy(pline->move + 1, line.move, line.count * sizeof(int));
+			pline->count = line.count + 1;
+		}
+	}
+
 	return alpha;
 }
