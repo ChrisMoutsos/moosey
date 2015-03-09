93f04d2dab3ea01bdf36fd5e9d069d966d549f9a
Chris Moutsos <ckm13d@my.fsu.edu>
2015-03-09 03:35:21 -0400

    Updated header in main.cpp, removed showMoveLists

--- src/main.cpp
+++ src/main.cpp
@@ -6,25 +6,17 @@
  ---------------------------------
 */
 
-/* ##Features##
- * - Negamax with alpha-beta pruning
- * - Quiescence search
- * - Principal variation search
- * - Iterated deepening,
- * - using old princ. var. as first nodes
- * - Aspiration window
- * - Null move heuristic
- * - (Removed history heuristic for now)
- * 
+/* 
  * ##To-Do List##
  * SDL:
  * - Add mute button
  * - Splash screen
  * FEN parsing
- * 
- * Known issues:
- * Algebraic notation:
+ * Threefold repetition
+ * Finish null move heuristic edge cases
+ * Verify that bot will deliver checkmates
  * Write dupMove code for Queens
+ * 
  */
 
 #include <iostream>
@@ -78,79 +70,10 @@ int main(int argc, char* args[]) {
 			}
 			board.handleInput(mF, mT, &e);
 			displayBoard(board, mF, mT);
-
-/*
-			if (!board.getSide()) {
-				board.handleInput(mF, mT, &e);
-				displayBoard(board, mF, mT);
-			}
-			else if (board.getSide()) {
-				std::cout << "white's turn\n";
-				if (board.getSideInCheckmate()) 
-					break;
-				botMove = 0;
-				botMove = think(board, 6);
-				if (!botMove) {
-					SDL_Delay(1000*60);
-					break;
-				}
-				board.setMove(botMove/100, botMove%100);
-				board.movePiece();
-				board.changeTurn();
-				board.genOrderedMoveList();
-				board.checkCheck(board.getSide());
-				displayBoard(board, mF, mT);
-				std::cout << "\n\n\n";
-//				showMoveLists(board);
-			}
-*/
 		}
 	}
 
 	SDL_Delay(2000);
 	close_SDL();
 	return 0;
-}
-
-void showMoveLists(Board &board) {
-	int mF, mT;
-	cout << "White movelist: " << endl;
-	for (int i = 0; i < board.getMoveListSize(WHITE); i++) {
-		mF = board.getFromMoveList(WHITE, i)/(100);
-		mT = board.getFromMoveList(WHITE, i)%(100);
-		cout << intToSquare(mF) << " to " << intToSquare(mT) << ", ";
-	}
-	cout << endl << endl;
-	/*
-	for (int i = wqR; i <= wPh; i++) {
-		cout << board.piece[i].getName() << ": ";
-		for (int j = 0; j < board.piece[i].getMoveListSize(); j++) {
-			if (board.piece[i].getFromMoveList(j) != null) {
-				mF = board.piece[i].getPos();
-				mT = board.piece[i].getFromMoveList(j)%100;
-				cout << intToSquare(mF) << " to " << intToSquare(mT) << ", ";
-			}
-		}
-		cout << "...";
-	}
-	cout << endl << endl;*/
-	cout << "Black movelist: " << endl;
-	for (int i = 0; i < board.getMoveListSize(BLACK); i++) {
-		mF = board.getFromMoveList(BLACK, i)/(100);
-		mT = board.getFromMoveList(BLACK, i)%(100);
-		cout << intToSquare(mF) << " to " << intToSquare(mT) << ", ";
-	}
-	cout << endl << endl;/*
-	for (int i = bqR; i <= bPh; i++) {
-		cout << board.piece[i].getName() << ": ";
-		for (int j = 0; j < board.piece[i].getMoveListSize(); j++) {
-			if (board.piece[i].getFromMoveList(j) != null) {
-				mF = board.piece[i].getPos();
-				mT = board.piece[i].getFromMoveList(j)%100;
-				cout << intToSquare(mF) << " to " << intToSquare(mT) << ", ";
-			}
-		}
-		cout << "...";
-	}
-	cout << endl;*/
 }
