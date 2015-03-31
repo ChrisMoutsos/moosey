4fe49533c6ba53a269ffe4f5b9cc36c147eee38e
Chris Moutsos <ckm13d@my.fsu.edu>
2015-03-30 22:18:37 -0400

    Better restart(). Bots now think to depth 'level'

--- src/board.cpp
+++ src/board.cpp
@@ -19,8 +19,8 @@ Board::Board() {
 	initializePieces();
 	placePiecesDefault();	
 	setSquarePositions();
-	setButtonPositions();
 	setSpriteClips();	
+	setButtons();
 }
 
 Board::Board(std::string FEN) {
@@ -29,9 +29,133 @@ Board::Board(std::string FEN) {
 	emptyBoard();
 	initializePieces();
 	setSquarePositions();
-	setButtonPositions();
 	setSpriteClips();	
+	setButtons();
 	placePieces(FEN);
+}
+
+void Board::setSquarePositions() {
+	//Set positions of the squares in the display
+	for (int i = 0; i < 64; i++) {
+		squares[i].setPos(BXSTART+(SQ_SIZE*(i%8)),
+				  BYSTART+B_SIZE-(SQ_SIZE*(i/8+1)));
+		squares[i].setSq(i+1);
+	}
+}
+
+void Board::setPiecesOnSquares() {
+	//Update all of the pieces the display squares are holding
+	for (int i = 0; i < 64; i++)
+		squares[i].setPiece(board120[from64(i+1)]);
+}
+
+void Board::initializeVars() {
+	moveFrom = moveTo = ply = 0;
+	side = WHITE;
+	whiteIsBot = false;
+	blackIsBot = true;
+	whiteCastled = blackCastled = false;
+	castling = sideInCheck = sideInCheckmate = 0;
+	whiteMaterial = 8*P_VAL + 2*(R_VAL+B_VAL+N_VAL) + Q_VAL + K_VAL;
+	blackMaterial = whiteMaterial;
+}
+
+void Board::emptyBoard() {
+	for (int i = 0; i < 120; i++) {
+		if (i < 21 || i > 98)
+			board120[i] = invalid;
+		else if (i%10 == 0 || i%10 == 9)
+			board120[i] = invalid;
+		else
+			board120[i] = empty;
+	}
+}
+
+void Board::placePiece(unsigned int p, unsigned int sq) {
+	piece[p].setPos(sq);
+	board120[sq] = p;
+}
+
+void Board::placePiecesDefault() {
+	for (int i = _A1; i <= _H1; i++)
+		placePiece(i-_A1, i);
+	for (int i = _A2; i <= _H2; i++) 
+		placePiece(i-_A2+8, i);
+	for (int i = _A8; i <= _H8; i++) 
+		placePiece(i-_A8+16, i);
+	for (int i = _A7; i <= _H7; i++) 
+		placePiece(i-_A7+24, i);
+}
+
+void Board::initializePieces() {
+	int v;
+	
+	//Set names, abbreviations, and values
+	for (int i = 0; i <= 16; i += 16) {
+		piece[wqR+i].setName("Rook");
+		piece[wqR+i].setAbbr('R' + 2*i);
+		piece[wqR+i].setValue(R_VAL);
+
+		piece[wqN+i].setName("Knight");
+		piece[wqN+i].setAbbr('N' + 2*i);
+		piece[wqN+i].setValue(N_VAL);
+
+		piece[wqB+i].setName("Bishop");
+		piece[wqB+i].setAbbr('B' + 2*i);
+		piece[wqB+i].setValue(B_VAL);
+
+		piece[wQ+i].setName("Queen");
+		piece[wQ+i].setAbbr('Q' + 2*i);
+		piece[wQ+i].setValue(Q_VAL);
+
+		piece[wK+i].setName("King");
+		piece[wK+i].setAbbr('K' + 2*i);
+		piece[wK+i].setValue(K_VAL);
+
+		piece[wkB+i].setName("Bishop");
+		piece[wkB+i].setAbbr('B' + 2*i);
+		piece[wkB+i].setValue(B_VAL);
+
+		piece[wkN+i].setName("Knight");
+		piece[wkN+i].setAbbr('N' + 2*i);
+		piece[wkN+i].setValue(N_VAL);
+
+		piece[wkR+i].setName("Rook");
+		piece[wkR+i].setAbbr('R' + 2*i);
+		piece[wkR+i].setValue(R_VAL);
+	}
+	for (int s = 0; s <= 16; s += 16) 
+		for (int i = 0; i < 8; i++) { 
+			piece[wPa+i+s].setName("Pawn");
+			piece[wPa+i+s].setAbbr('P' + 2*s);
+			piece[wPa+i+s].setValue(P_VAL);
+		}
+
+	//Set colors
+	for (int i = wqR; i <= wPh; i++)
+		piece[i].setColor(WHITE);
+	for (int i = bqR; i <= bPh; i++)
+		piece[i].setColor(BLACK);
+
+	//Create moveLists
+	for (int i = wqR; i <= bPh; i++) {
+		piece[i].freeMoveList();
+	
+		v = piece[i].getValue();
+		if (v == K_VAL)
+			piece[i].setMoveListSize(10);
+		else if (v == Q_VAL)
+			piece[i].setMoveListSize(27);
+		else if (v == R_VAL)
+			piece[i].setMoveListSize(14);
+		else if (v == B_VAL)
+			piece[i].setMoveListSize(13);
+		else if (v == N_VAL)
+			piece[i].setMoveListSize(8);
+		else if (v == P_VAL)
+			piece[i].setMoveListSize(4);
+		piece[i].setMoveList(new int[piece[i].getMoveListSize()]);
+    }
 }
 
 void Board::placePieces(std::string FEN) {
@@ -163,138 +287,11 @@ void Board::placePieces(std::string FEN) {
 	
 }
 
-void Board::setSquarePositions() {
-	//Set positions of the squares in the display
-	for (int i = 0; i < 64; i++) {
-		squares[i].setPos(BXSTART+(SQ_SIZE*(i%8)),
-				  BYSTART+B_SIZE-(SQ_SIZE*(i/8+1)));
-		squares[i].setSq(i+1);
-	}
-}
-
-void Board::setPiecesOnSquares() {
-	//Update all of the pieces the display squares are holding
-	for (int i = 0; i < 64; i++)
-		squares[i].setPiece(board120[from64(i+1)]);
-}
-
-void Board::initializeVars() {
-	moveFrom = moveTo = ply = 0;
-	side = WHITE;
-	whiteCastled = blackCastled = false;
-	castling = sideInCheck = sideInCheckmate = 0;
-	whiteMaterial = 8*P_VAL + 2*(R_VAL+B_VAL+N_VAL) + Q_VAL + K_VAL;
-	blackMaterial = whiteMaterial;
-}
-
-void Board::emptyBoard() {
-	for (int i = 0; i < 120; i++) {
-		if (i < 21 || i > 98)
-			board120[i] = invalid;
-		else if (i%10 == 0 || i%10 == 9)
-			board120[i] = invalid;
-		else
-			board120[i] = empty;
-	}
-}
-
-void Board::placePiece(unsigned int p, unsigned int sq) {
-	piece[p].setPos(sq);
-	board120[sq] = p;
-}
-
-void Board::placePiecesDefault() {
-	for (int i = _A1; i <= _H1; i++)
-		placePiece(i-_A1, i);
-	for (int i = _A2; i <= _H2; i++) 
-		placePiece(i-_A2+8, i);
-	for (int i = _A8; i <= _H8; i++) 
-		placePiece(i-_A8+16, i);
-	for (int i = _A7; i <= _H7; i++) 
-		placePiece(i-_A7+24, i);
-}
-
-void Board::initializePieces() {
-	int v;
-	
-	//Set names, abbreviations, and values
-	for (int i = 0; i <= 16; i += 16) {
-		piece[wqR+i].setName("Rook");
-		piece[wqR+i].setAbbr('R' + 2*i);
-		piece[wqR+i].setValue(R_VAL);
-
-		piece[wqN+i].setName("Knight");
-		piece[wqN+i].setAbbr('N' + 2*i);
-		piece[wqN+i].setValue(N_VAL);
-
-		piece[wqB+i].setName("Bishop");
-		piece[wqB+i].setAbbr('B' + 2*i);
-		piece[wqB+i].setValue(B_VAL);
-
-		piece[wQ+i].setName("Queen");
-		piece[wQ+i].setAbbr('Q' + 2*i);
-		piece[wQ+i].setValue(Q_VAL);
-
-		piece[wK+i].setName("King");
-		piece[wK+i].setAbbr('K' + 2*i);
-		piece[wK+i].setValue(K_VAL);
-
-		piece[wkB+i].setName("Bishop");
-		piece[wkB+i].setAbbr('B' + 2*i);
-		piece[wkB+i].setValue(B_VAL);
-
-		piece[wkN+i].setName("Knight");
-		piece[wkN+i].setAbbr('N' + 2*i);
-		piece[wkN+i].setValue(N_VAL);
-
-		piece[wkR+i].setName("Rook");
-		piece[wkR+i].setAbbr('R' + 2*i);
-		piece[wkR+i].setValue(R_VAL);
-	}
-	for (int s = 0; s <= 16; s += 16) 
-		for (int i = 0; i < 8; i++) { 
-			piece[wPa+i+s].setName("Pawn");
-			piece[wPa+i+s].setAbbr('P' + 2*s);
-			piece[wPa+i+s].setValue(P_VAL);
-		}
-
-	//Set colors
-	for (int i = wqR; i <= wPh; i++)
-		piece[i].setColor(WHITE);
-	for (int i = bqR; i <= bPh; i++)
-		piece[i].setColor(BLACK);
-
-	//Create moveLists
-	for (int i = wqR; i <= bPh; i++) {
-		piece[i].freeMoveList();
-	
-		v = piece[i].getValue();
-		if (v == K_VAL)
-			piece[i].setMoveListSize(10);
-		else if (v == Q_VAL)
-			piece[i].setMoveListSize(27);
-		else if (v == R_VAL)
-			piece[i].setMoveListSize(14);
-		else if (v == B_VAL)
-			piece[i].setMoveListSize(13);
-		else if (v == N_VAL)
-			piece[i].setMoveListSize(8);
-		else if (v == P_VAL)
-			piece[i].setMoveListSize(4);
-		piece[i].setMoveList(new int[piece[i].getMoveListSize()]);
-    }
-}
-
 void Board::handleInput(int& mF, int& mT, SDL_Event* e) {
-	bool clearMFMT = false;
-
-	for (int i = 0; i < 2; i++) 
+	for (int i = 0; i < 26; i++) 
 		if (buttons[i].handleEvent(e, *this)) 
-			clearMFMT = true;
-	if (clearMFMT) {
-		mF = -1;
-		mT = -1;
-	}
+			mF = mT = -1;
+
 	for (int i = 0; i < 64; i++)
 		squares[i].handleEvent(e, mF, mT, side);
 	
@@ -309,23 +306,25 @@ void Board::handleInput(int& mF, int& mT, SDL_Event* e) {
 			genOrderedMoveList();
 			checkCheck(getSide());
 			std::cout << "Current FEN: " << getFEN() << '\n';
-			std::cout << "drawCheck?: " << drawCheck() << '\n';
+			if (drawCheck())
+				std::cout << "Draw\n";
 		}
-		mF = -1;
-		mT = -1;
+		mF = mT = -1;
 	}
 }
 
 void Board::botMove() {
+	if (sideInCheckmate)
+		return;
 	std::cout << "Thinking for ";
 	side ? std::cout << "White..." : std::cout << "Black...";
 	std::cout << " (ply " << ply+1 << ", move " << ply/2 + 1 << ")\n";
 	displayBotText(*this);
 	int move = 0;
 	if (side)
-		move = whiteBot.think(*this, 7);
+		move = whiteBot.think(*this, whiteBot.getLevel());
 	else
-		move = blackBot.think(*this, 7);
+		move = blackBot.think(*this, blackBot.getLevel());
 	setMove(move/100, move%100);
 	movePiece();
 	if (!muted)
@@ -335,9 +334,9 @@ void Board::botMove() {
 	genOrderedMoveList();
 	checkCheck(side);
 	std::cout << "White material: " << whiteMaterial << " Black material: " << blackMaterial << '\n';
+	std::cout << "Current FEN: " << getFEN() << '\n';
 	if (drawCheck())
 		std::cout << "Draw!\n";
-	std::cout << "Current FEN: " << getFEN() << '\n';
 }
 
 void Board::changeTurn() {
@@ -367,24 +366,23 @@ void Board::undoMove() {
 }
 
 void Board::restart() {
-	//Restart to default position
-	if (movesMade.size() == 0) {
-		if (getFEN() != defaultFEN) {
-			initializeVars();
-			emptyBoard();
-			initializePieces();
-			placePiecesDefault();
-		}
+	if (!start) return;
+	
+	//If we didn't load from a FEN
+	if (ply == (int)movesMade.size()) {
+		while (movesMade.size())
+			undoMove();
+		start = false;
+		std::cout << "\nRestarted game\n";
 	}
-	//Restart to loaded FEN
+	//If we loaded from a FEN
 	else {
 		while (movesMade.size())
 			undoMove();	
-		moveTo = null;
-		moveFrom = null;
+		moveTo = moveFrom = 0;
+		std::cout << "\nReloaded FEN\n";
 	}
 
-	std::cout << "\nRestarted game\n";
 }
 
 //ACCESSORS
@@ -495,8 +493,7 @@ int Board::getNumMovesMade() const {
 }
 
 int Board::getLastMove() const {
-	if (movesMade.size())
-		return movesMade.back();
+	if (movesMade.size()) return movesMade.back();
 	return 0;
 }
 
