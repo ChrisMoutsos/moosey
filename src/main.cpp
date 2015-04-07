d5dc165114796af36e0c4c062d8d3a0ec117a4bd
Chris Moutsos <ckm13d@my.fsu.edu>
2015-04-06 22:19:09 -0400

    Updated to-do list

--- src/main.cpp
+++ src/main.cpp
@@ -11,7 +11,7 @@
  * Add en passants to quiescent search maybe
  * Make sure killer moves are being implemented correctly
  * Write dupMove code for Queens
- * Make sure threefold repetition detection works
+ * Make sure FEN loading works perfectly
  * 
  */
 
@@ -41,14 +41,10 @@ int main(int argc, char* args[]) {
 	int mF = -1, mT = -1;
 	SDL_Event e; //Event handler
 
-//	Board board("r1bqkbnr/ppp2ppp/2np4/4p3/2B1P3/2N2N2/PPPP1PPP/R1BQK2R b KQkq - 1 4");
-//	Board board("8/8/p1p5/1p5p/1P5p/8/PPP2K1p/4R1rk w - - 0 1");
 	Board board;
-//	Board board("r2b1rk1/pbpq1ppp/2pp1n2/5N2/4P3/2N2Q2/PPP2PPP/R1B1R1K1 w - - 5 13");
-//	Board board("6k1/ppp2ppp/8/r7/8/3KN3/8/8 b - - 0 2");
-//	Board board("8/p3R2k/6pp/2p3r1/3pQ3/3P1P2/PPPq2PP/4R1K1 b - - 1 35");
-	std::cout << "Current FEN: " << board.getFEN() << '\n';
-	std::cout << "Current Zobrist: " << board.getZobrist() << '\n';
+
+	std::cout << "Current FEN (start): " << board.getFEN() << '\n';
+	std::cout << "Current Zobrist (start): " << board.getZobrist() << '\n';
 
 	while (!quit) {
 		while (SDL_PollEvent(&e)) {
